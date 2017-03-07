c7d5f4ad162c96742ee9fb3fcd73bcfee83f8ffc
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-03-07 14:22:50 -0800

    Use HTTPS.

--- listMyBuilds.sh
+++ listMyBuilds.sh
@@ -8,7 +8,7 @@ sbopkg -r
 
 cd /var/lib/sbopkg/SBo/
 
-grep -r 'MAINTAINER="Ryan P.C. McQuen"' . | cut -d/ -f3-3 | sort | awk '{print "http://slackbuilds.org/apps/"$0"/\n"}' > ~/ryanpc-slackbuilds/README.md
+grep -r 'MAINTAINER="Ryan P.C. McQuen"' . | cut -d/ -f3-3 | sort | awk '{print "https://slackbuilds.org/apps/"$0"/\n"}' > ~/ryanpc-slackbuilds/README.md
 
 cd ~/ryanpc-slackbuilds/
 
